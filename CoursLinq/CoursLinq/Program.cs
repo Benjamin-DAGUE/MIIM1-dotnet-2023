f57bd2e6bc48741e72c90c325452aee932da1d75
Benjamin DAGUE <bdague@grindboxlab.fr>
2023-09-13 18:46:55 +0200

    Requêtes linq

--- CoursLinq/CoursLinq/Program.cs
+++ CoursLinq/CoursLinq/Program.cs
@@ -164,26 +164,7 @@ namespace CoursLinq
 
             Random random = new Random();
 
-            int count = 1000;
-            //List<Person> persons = new List<Person>();
-
-            //for (int i = 0; i < count; i++)
-            //{
-            //    string firstName = firstNames.ElementAt(random.Next(0, firstNames.Count - 1));
-            //    string lastName = lastNames.ElementAt(random.Next(0, lastNames.Count - 1));
-
-            //    Person p = new Person()
-            //    {
-            //        FirstName = firstName,
-            //        LastName = lastName,
-            //        EMailAddress = $"{firstName}.{lastName}{emailExts.ElementAt(random.Next(0, emailExts.Count))}",
-            //        Birthdate = new DateTime(1950,1,1).AddDays(random.Next(0, 21915))
-            //    };
-
-            //    persons.Add(p);
-            //}
-
-            List<Person> people = Enumerable.Range(0, count)
+            List<Person> people = Enumerable.Range(0, 100)
                 .Select(i =>
                 {
                     string firstName = firstNames.ElementAt(random.Next(0, firstNames.Count - 1));
@@ -209,6 +190,10 @@ namespace CoursLinq
             #region 1 - Afficher les personnes nées avant l'an 2000
 
             Console.WriteLine("1 - Afficher les personnes nées avant l'an 2000");
+            people                                              //On part de la liste des personnes.
+                .Where(p => p.Birthdate.Year < 2000)            //On filtre pour conserver les personnes nées avant l'an 2000.
+                .ToList()                                       //On exécute la requête en créant une liste du réslutat.
+                .ForEach(p => Console.WriteLine(p.FullName));   //Pour chaque personne dans la liste du résultat, on affiche son nom et son prénom.
 
             #endregion
 
@@ -217,6 +202,12 @@ namespace CoursLinq
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("2 - Afficher les personnes nées en janvier et en février");
+            people
+                //.Where(p => new[] { 1, 2 }.Contains(p.Birthdate.Month))
+                .Where(p => p.Birthdate.Month <= 2)
+                //.Where(p => p.Birthdate.Month <= 3)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.FullName));
 
             #endregion
 
@@ -225,6 +216,12 @@ namespace CoursLinq
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("3 - Afficher les personnes nées un samedi ou un dimanche");
+            people
+                .Where(p => new[] { DayOfWeek.Saturday, DayOfWeek.Sunday
+}.Contains(p.Birthdate.DayOfWeek))
+                //.Where(p => p.Birthdate.DayOfWeek == DayOfWeek.Saturday || p.Birthdate.DayOfWeek == DayOfWeek.Sunday)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.FullName));
 
             #endregion
 
@@ -233,6 +230,10 @@ namespace CoursLinq
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("4 - Afficher les personnes pour lesquelles le prénom a plus de 7 caractères");
+            people
+                .Where(p => p.FirstName.Length > 7)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.FullName));
 
             #endregion
 
@@ -241,6 +242,12 @@ namespace CoursLinq
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("5 - Afficher les personnes nées en janvier et en février et pour lesquelles le prénom a plus de 7 caractères");
+            people
+                .Where(p => p.Birthdate.Month <= 2 && p.FirstName.Length > 7)
+                //.Where(p => p.Birthdate.Month <= 2)
+                //.Where(p => p.FirstName.Length > 7)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.FullName));
 
             #endregion
 
@@ -249,7 +256,7 @@ namespace CoursLinq
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("6 - Calculer la moyenne d'age des personnes");
-            double averageAge = 0;
+            double averageAge = people.Average(p => p.Birthdate.CalculateAge());
             Console.WriteLine("L'âge moyen est : " + averageAge);
 
             #endregion
@@ -259,6 +266,10 @@ namespace CoursLinq
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("7 - Afficher les personnes de la plus ancienne à la plus jeune");
+            people
+                .OrderBy(p => p.Birthdate)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.FullName));
 
             #endregion
 
@@ -267,6 +278,12 @@ namespace CoursLinq
             Console.WriteLine();
             Console.WriteLine();
             Console.WriteLine("8 - Afficher les personnes dont l'age est supérieur à la moyenne d'âge");
+            people
+                //Cette méthode calcul la moyenne d'âge plusieurs fois (une foi par personne présente dans la liste)
+                //.Where(p => p.Birthdate.CalculateAge() > people.Average(p => p.Birthdate.CalculateAge()))
+                .Where(p => p.Birthdate.CalculateAge() > averageAge)
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.FullName));
 
             #endregion
 
@@ -277,6 +294,39 @@ namespace CoursLinq
             Console.WriteLine("9 - Saisisez une chaîne et afficher les personnes dont le nom contient la recherche");
             Console.Write("Votre recherche : ");
             string search = Console.ReadLine();
+            people
+                .Where(p => p.FullName.ToLower().Contains(search?.ToLower()))
+                .ToList()
+                .ForEach(p => Console.WriteLine(p.FullName));
+
+            #endregion
+
+            #region Grouping
+
+            //Linq prend en charge le regroupement avec GroupBy.
+            //GroupBy retourne un IGrouping<K, T> :
+            //  - K est le type de la clef de regroupement
+            //  - T est le type des éléments regroupés
+            foreach (IGrouping<char, Person> group in people.GroupBy(p => p.FullName[0]))
+            {
+                Console.WriteLine("--------");
+                Console.WriteLine(group.Key); //La clef de regroupement est accessible avec group.Key
+                Console.WriteLine("--------");
+
+                //La liste de résultat d'un group peut être énumré avec un foreach (Person p in group)
+                //il est possible d'imbriquer deux GroupBy
+                foreach (IGrouping<int, Person> group2 in group.GroupBy(g => g.Birthdate.Year))
+                {
+                    Console.WriteLine("--------");
+                    Console.WriteLine(group2.Key);
+                    Console.WriteLine("--------");
+
+                    foreach (Person person in group2)
+                    {
+                        Console.WriteLine(person.FullName);
+                    }
+                }
+            }
 
             #endregion
 
