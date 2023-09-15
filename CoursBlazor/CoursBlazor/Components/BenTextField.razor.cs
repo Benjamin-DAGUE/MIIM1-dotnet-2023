dc6c1ba78fa4f5706e5cca56c6496323304f521d
Benjamin DAGUE <bdague@grindboxlab.fr>
2023-09-15 13:46:15 +0200

    Ajout cours blazor

---
+++ CoursBlazor/CoursBlazor/Components/BenTextField.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace CoursBlazor.Components;
+
+public partial class BenTextField
+{
+    [Parameter]
+    public string Text { get; set; } = string.Empty;
+
+    [Parameter]
+    public EventCallback<string> TextChanged { get; set; }
+
+    private void OnTextChange()
+    {
+        TextChanged.InvokeAsync(Text);
+    }
+}
---
+++ CoursBlazor/CoursBlazor/Pages/Index.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace CoursBlazor.Pages;
+
+public partial class Index
+{
+    private void OnClick()
+    {
+        Console.WriteLine("Hello world");
+
+        Snackbar.Add("Hello world !");
+    }
+}
